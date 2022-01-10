01166b4ff63c78900c5fa5da335efb3798755d8f
auroraesposito <96207112+auroraesposito@users.noreply.github.com>
2022-01-10 17:35:28 +0100

    Add files via upload

---
+++ Prj09_Zaino/src/model/Zaino.java
@@ -0,0 +1,36 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Zaino {
+
+	private ArrayList<Oggetto> oggetti;
+
+	public Zaino() {
+		this.oggetti = new ArrayList<Oggetto>();
+	}
+	
+	public double getPesoZaino() {
+		
+		double peso = 0;
+		
+		for (Oggetto oggetto : oggetti) {
+			peso += oggetto.getPeso();
+		}
+	
+		return peso;
+	}
+	
+	public ArrayList<Oggetto> getOggetti() {
+		return oggetti;
+	}
+
+	@Override
+	public String toString() {
+		return "Zaino [oggetti=" + oggetti + "]";
+	}
+	
+	
+	
+}

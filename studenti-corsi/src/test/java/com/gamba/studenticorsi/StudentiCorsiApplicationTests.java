87dcc1bce580ad496457e6dfbf240dcf17cc7517
auroraesposito <96207112+auroraesposito@users.noreply.github.com>
2022-03-10 14:22:34 +0100

    Add files via upload

---
+++ studenti-corsi/src/test/java/com/gamba/studenticorsi/StudentiCorsiApplicationTests.java
@@ -0,0 +1,26 @@
+package com.gamba.studenticorsi;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.gamba.studenticorsi.entities.Esame;
+import com.gamba.studenticorsi.services.ScuolaService;
+
+@SpringBootTest
+class StudentiCorsiApplicationTests {
+
+	@Autowired
+	private ScuolaService service;
+	
+	@Test
+	void contextLoads() {
+		service.addEsame(new Esame ("Francese A", "FRA01", 12));
+		
+
+		assertTrue(service.getEsameById(1).getCrediti()==10);
+	}
+
+}
